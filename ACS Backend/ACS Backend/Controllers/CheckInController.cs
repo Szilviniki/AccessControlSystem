0a52be165588619c8b678f51576ecd710d35839b
oliver balassa <balassa.oliver@outlook.com>
2024-02-11 22:31:17 +0100

    Added,fixed routes, corrected entity, added controller
    --Controllers--
    +CheckIn
    --Routes--
    +students/GetExtended
     >now needs card id instead of guid
     >fixed query
     +CheckIn/IsWorking
      >Checks for live Db connection on backend
    --Fixes--
    >fixed StudentExtended entity
    >fixed view querying

---
+++ ACS Backend/ACS Backend/Controllers/CheckInController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ACS_Backend.Controllers;
+[Route("api/v1/[controller]")]
+public class CheckInController : ControllerBase
+{
+    private SQL _sql;
+
+    public CheckInController(SQL sql)
+    {
+        _sql = sql;
+    }
+    [HttpGet("IsWorking")]
+    public IActionResult DbConnectionAlive()
+    {
+        return StatusCode(!_sql.Database.CanConnect() ? 503 : 204);
+    }
+}
