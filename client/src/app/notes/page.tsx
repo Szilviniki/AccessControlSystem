f2a250eec433045b9ca3587a9a6df07ff2a6569b
Niki <szilviniki9@gmail.com>
2024-03-22 11:38:09 +0100

    .

--- client/src/app/notes/page.tsx
+++ client/src/app/notes/page.tsx
@@ -1,5 +1,7 @@
-'use client'
 import React from "react";
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
+import MainTemplate from "@/app/templates/MainTemplate";
 
 
 export default function NotesPage() {
