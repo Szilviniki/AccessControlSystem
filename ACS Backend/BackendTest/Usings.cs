2889ca53e10c607921a6f39b1affa16272d6b7ec
oliver balassa <balassa.oliver@outlook.com>
2024-03-25 11:41:56 +0100

    Added unit testing project, made tests for login and adding students

---
+++ ACS Backend/BackendTest/Usings.cs
@@ -0,0 +1,7 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using ACS_Backend.Services;
+global using ACS_Backend.Model;
+global using ACS_Backend.Interfaces;
+global using ACS_Backend.Exceptions;
+global using DB_Module;
+global using DB_Module.Models;
