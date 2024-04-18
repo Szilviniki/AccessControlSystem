da472e33b9fda0601342ddd99d5059921f3f16c7
oliver balassa <balassa.oliver@outlook.com>
2024-04-18 15:07:37 +0200

    fixed StudentsController.cs

--- ACS Backend/ACS Backend/Controllers/StudentsController.cs
+++ ACS Backend/ACS Backend/Controllers/StudentsController.cs
@@ -6,72 +6,72 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 
-namespace ACS_Backend.Controllers { 
-
-[ApiController]
-[EnableCors]
-[Route("api/v1/[controller]")]
-[Authorize]
-public class StudentsController : ControllerBase
+namespace ACS_Backend.Controllers
 {
-    private IStudentService _studentService;
-
-    public StudentsController(IStudentService studentService)
+    [ApiController]
+    [EnableCors]
+    [Route("api/v1/[controller]")]
+    [Authorize]
+    public class StudentsController : ControllerBase
     {
-        _studentService = studentService;
-    }
+        private IStudentService _studentService;
 
-    [HttpGet("Get/{id}")]
-    public IActionResult Get(Guid id)
-    {
-        var student = _studentService.GetStudent(id);
-        var res = new GenericResponseModel<Student> { Data = student, QueryIsSuccess = true };
-        return Ok(res);
-    }
+        public StudentsController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet("Get/{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var student = _studentService.GetStudent(id);
+            var res = new GenericResponseModel<Student> { Data = student, QueryIsSuccess = true };
+            return Ok(res);
+        }
 
 
-    [HttpGet("GetAll")]
-    public IActionResult GetAll()
-    {
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
             var res = new GenericResponseModel<Array> { Data = _studentService.GetAllStudents() };
             return Ok(res);
-    }
+        }
 
-    [HttpPut("Add")]
-    public async Task<IActionResult> Add([FromBody] Student student)
-    {
-        await _studentService.AddStudent(student);
-        return StatusCode(201);
-    }
-
-    [HttpPost("Update/{id:Guid}")]
-    public async Task<IActionResult> Update(Guid id, [FromBody] UpdateStudentModel updatedStudent)
-    {
-        await _studentService.UpdateStudent(updatedStudent, id);
-        return Ok(new GenericResponseModel<string>(){Message = "Student updated", QueryIsSuccess = true});
-    }
-
-    [HttpDelete("Delete/{id}")]
-    public async Task<IActionResult> Delete(Guid id)
-    {
-        await _studentService.RemoveStudent(id);
-        return Ok();
-    }
-
-    [HttpPut("AddWithParent")]
-    public async Task<IActionResult> AddWithParent([FromBody] AddStudentWithParentModel model)
-    {
-        var parent = new Guardian()
+        [HttpPut("Add")]
+        public async Task<IActionResult> Add([FromBody] Student student)
         {
-            Email = model.ParentEmail,
-            Phone = model.ParentPhone,
-            Name = model.ParentName,
-            Id = Guid.NewGuid()
-        };
+            await _studentService.AddStudent(student);
+            return StatusCode(201);
+        }
 
-        Random random = new Random();
+        [HttpPost("Update/{id:Guid}")]
+        public async Task<IActionResult> Update(Guid id, [FromBody] UpdateStudentModel updatedStudent)
+        {
+            await _studentService.UpdateStudent(updatedStudent, id);
+            return Ok(new GenericResponseModel<string>() { Message = "Student updated", QueryIsSuccess = true });
+        }
 
-            var cardID = random.Next(100000,999999);
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _studentService.RemoveStudent(id);
+            return Ok();
+        }
+
+        [HttpPut("AddWithParent")]
+        public async Task<IActionResult> AddWithParent([FromBody] AddStudentWithParentModel model)
+        {
+            var parent = new Guardian()
+            {
+                Email = model.ParentEmail,
+                Phone = model.ParentPhone,
+                Name = model.ParentName,
+                Id = Guid.NewGuid()
+            };
+
+            Random random = new Random();
+
+            var cardID = random.Next(100000, 999999);
 
             var student = new Student()
             {
@@ -85,19 +85,20 @@ public class StudentsController : ControllerBase
             await _studentService.AddStudentWithParent(student, parent);
             return StatusCode(201);
         }
-    }
-    
-    [HttpPost("AddNote")]
-    public async Task<IActionResult> AddNoteToStudent([FromBody] Note note)
-    {
-        await _studentService.AddNoteToStudent(note);
-        return StatusCode(201);
-    }
-    
-    [HttpDelete("RemoveNote/{noteId}")]
-    public async Task<IActionResult> RemoveNoteFromStudent(int noteId)
-    {
-        await _studentService.RemoveNoteFromStudent(noteId);
-        return Ok();
+
+
+        [HttpPost("AddNote")]
+        public async Task<IActionResult> AddNoteToStudent([FromBody] Note note)
+        {
+            await _studentService.AddNoteToStudent(note);
+            return StatusCode(201);
+        }
+
+        [HttpDelete("RemoveNote/{noteId}")]
+        public async Task<IActionResult> RemoveNoteFromStudent(int noteId)
+        {
+            await _studentService.RemoveNoteFromStudent(noteId);
+            return Ok();
+        }
     }
 }
